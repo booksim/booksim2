d35a54dff5a1c5285aea8a7f5c83addeb04ee387
Daniel Becker <dub@cva.stanford.edu>
2010-10-11 18:26:56 +0000

    STLize PipelineFIFO class
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@2641 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/pipefifo.hpp
+++ src/pipefifo.hpp
@@ -31,6 +31,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef _PIPEFIFO_HPP_
 #define _PIPEFIFO_HPP_
 
+#include <vector>
+
 #include "module.hpp"
 
 template<class T> class PipelineFIFO : public Module {
@@ -40,7 +42,7 @@ template<class T> class PipelineFIFO : public Module {
   int _pipe_len;
   int _pipe_ptr;
   
-  T ***_data;
+  vector<vector<T*> > _data;
 
 public:
   PipelineFIFO( Module *parent, const string& name, int lanes, int depth );
@@ -63,22 +65,14 @@ template<class T> PipelineFIFO<T>::PipelineFIFO( Module *parent,
   _pipe_len = depth + 1;
   _pipe_ptr = 0;
 
-  _data = new T ** [_lanes];
+  _data.resize(_lanes);
   for ( int l = 0; l < _lanes; ++l ) {
-    _data[l] = new T * [_pipe_len];
-
-    for ( int d = 0; d < _pipe_len; ++d ) {
-      _data[l][d] = 0;
-    }
+    _data[l].resize(_pipe_len, 0);
   }
 }
 
 template<class T> PipelineFIFO<T>::~PipelineFIFO( ) 
 {
-  for ( int l = 0; l < _lanes; ++l ) {
-    delete [] _data[l];
-  }
-  delete [] _data;
 }
 
 template<class T> void PipelineFIFO<T>::Write( T* val, int lane )
