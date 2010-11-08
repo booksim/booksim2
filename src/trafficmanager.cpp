0de6037cb5201edfd12abef9f5b12d79faee9ea9
Daniel Becker <dub@cva.stanford.edu>
2010-11-08 07:11:15 +0000

    skip unnecessary iterations
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@2780 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/trafficmanager.cpp
+++ src/trafficmanager.cpp
@@ -906,7 +906,7 @@ void TrafficManager::_BatchInject(){
     for (int i = 0; i < _duplicate_networks; ++i) {
       int highest_class = 0;
       // Now just find which is the highest_class.
-      for (int c = _classes - 1; c >= 0; --c) {
+      for (int c = _classes - 1; c > 0; --c) {
         if (_class_array[i][c]) {
           highest_class = c;
           break;
@@ -1025,9 +1025,9 @@ void TrafficManager::_NormalInject(){
     for (int i = 0; i < _duplicate_networks; ++i) {
       int highest_class = 0;
       // Now just find which is the highest_class.
-      for (int a = _classes - 1; a >= 0; --a) {
-	if (_class_array[i][a]) {
-	  highest_class = a;
+      for (int c = _classes - 1; c > 0; --c) {
+	if (_class_array[i][c]) {
+	  highest_class = c;
 	  break;
 	}
       }
