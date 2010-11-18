2d3d687fb85c4d5f6fde945a62fc3ebbeaf73c1e
Daniel Becker <dub@cva.stanford.edu>
2010-11-18 20:21:54 +0000

    update copyright notice
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@2933 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/routers/iq_router_split.cpp
+++ src/routers/iq_router_split.cpp
@@ -1,7 +1,7 @@
 // $Id: iq_router_split.cpp -1   $
 
 /*
-Copyright (c) 2007-2009, Trustees of The Leland Stanford Junior University
+Copyright (c) 2007-2010, Trustees of The Leland Stanford Junior University
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
