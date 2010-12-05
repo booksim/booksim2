ea06fe19e19881155bf26251b4d6cd14df5c0a45
Daniel Becker <dub@cva.stanford.edu>
2010-12-05 00:02:01 +0000

    fix polarity of BufferState::HasCreditFor() method
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@3078 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/buffer_state.cpp
+++ src/buffer_state.cpp
@@ -167,10 +167,10 @@ bool BufferState::IsAvailableFor( int vc ) const
 bool BufferState::HasCreditFor( int vc ) const
 {
   assert( ( vc >= 0 ) && ( vc < _vcs ) );
-  return ( ( _cur_occupied[vc] >= _vc_buf_size ) &&
-	   ( ( _shared_occupied >= _shared_buf_size ) ||
+  return ( ( _cur_occupied[vc] < _vc_buf_size ) ||
+	   ( ( _shared_occupied < _shared_buf_size ) &&
 	     ( _dynamic_sharing && ( _active_vcs > 0 ) &&
-	       ( ( _cur_occupied[vc] - _vc_buf_size ) >= 
+	       ( ( _cur_occupied[vc] - _vc_buf_size ) < 
 		 ( _shared_buf_size / _active_vcs ) ) ) ) );
 }
 
