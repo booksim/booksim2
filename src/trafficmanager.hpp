7beea71a03aa70b9a159c96c5c7b79c067a80505
Daniel Becker <dub@cva.stanford.edu>
2011-04-30 03:39:39 +0000

    remove obsolete member variables
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@3464 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/trafficmanager.hpp
+++ src/trafficmanager.hpp
@@ -142,12 +142,6 @@ protected:
   int _last_id;
   int _last_pid;
 
-  // ============voq mode =============================
-
-  vector<vector<list<Flit*> > > _voq;
-  vector<list<int> > _active_list;
-  vector<vector<bool> > _active_vc;
-  
   // ============ Statistics ============
 
   vector<Stats *> _plat_stats;     
