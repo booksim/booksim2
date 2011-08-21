7445d954a54a5e226851b71b2be738d66e26ba69
Daniel Becker <dub@cva.stanford.edu>
2011-08-21 01:35:16 +0000

    add network latency stat
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@3895 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/flit.cpp
+++ src/flit.cpp
@@ -52,7 +52,7 @@ ostream& operator<<( ostream& os, const Flit& f )
      << " Head: " << f.head
      << " Tail: " << f.tail << endl;
   os << "  Source: " << f.src << "  Dest: " << f.dest << " Intm: "<<f.intm<<endl;
-  os << "  Creation time: " << f.ctime << " Transaction creation time: " << f.ttime << " Arrival time: " << f.atime << " Phase: "<<f.ph<< endl;
+  os << "  Creation time: " << f.ctime << " Transaction creation time: " << f.ttime << " Injection time: " << f.itime << " Arrival time: " << f.atime << " Phase: "<<f.ph<< endl;
   os << "  VC: " << f.vc << endl;
   return os;
 }
@@ -70,6 +70,7 @@ void Flit::Reset()
   head      = false ;
   tail      = false ;
   ctime     = -1 ;
+  itime     = -1 ;
   ttime     = -1 ;
   atime     = -1 ;
   id        = -1 ;
