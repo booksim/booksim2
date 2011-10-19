e9fbfd441de846d9cacb44a750e816bb3d511005
Daniel Becker <dub@cva.stanford.edu>
2011-10-19 00:08:53 +0000

    add option to prioritize empty VCs during VC allocation
    git-svn-id: https://nocs.stanford.edu/cgi-bin/svn.cgi/booksim2.0/trunk@4062 ce7aba0d-9f81-40a6-a495-cecae84b0515

--- src/booksim_config.cpp
+++ src/booksim_config.cpp
@@ -119,6 +119,7 @@ BookSimConfig::BookSimConfig( )
 
   _int_map["wait_for_tail_credit"] = 0; // reallocate a VC before a tail credit?
   _int_map["vc_busy_when_full"] = 0; // mark VCs as in use when they have no credit available
+  _int_map["vc_prioritize_empty"] = 0; // prioritize empty VCs over non-empty ones in VC allocation
   _int_map["vc_priority_donation"] = 0; // allow high-priority flits to donate their priority to low-priority that they are queued up behind
   _int_map["replies_inherit_priority"] = 0; // whenusing request-reply traffic (use_read_write=1) with age-based priority, make replies inherit their corresponding requests' age
 
